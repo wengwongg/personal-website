b6842ea718e9ae395d5a2242384d9dd290ce1589
Weng Wong (George) Hum <124537582+wengwongg@users.noreply.github.com>
2025-04-10 08:34:15 +0100

    changed border

--- components/Portrait.tsx
+++ components/Portrait.tsx
@@ -8,7 +8,7 @@ interface PictureProps {
 export default function Portrait({ src, alt }: PictureProps): JSX.Element {
   return (
     <Image
-      className="rounded-lg outline outline-8 outline-white shadow-xl rotate-2 mb-4 z-0 w-auto h-auto"
+      className="rounded-lg outline outline-4 outline-white shadow-xl rotate-2 mb-4 z-0 w-auto h-auto"
       src={src}
       alt={alt}
       width={125}
